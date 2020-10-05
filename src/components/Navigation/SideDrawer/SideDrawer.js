6e2858db8d095536ab3d85bee0987e8f88eadd76
Jeffreyx38 <Jeffreyx38@gmail.com>
2020-10-05 11:44:04 -0400

    added backdrop and toggle button for mobile

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
 import Logo from '../../Logo/Logo';
-
+import Aux from '../../../hoc/Aux'
 import NavigationItems from '../NavigationItems/NavigationItems'
-
+import Backdrop from '../../UI/Backdrop/Backdrop'
 import classes from './SideDrawer.module.css'
-const sideDrawer = (props) =>{
-
-    return(
-        <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <Logo height="11%" />
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
