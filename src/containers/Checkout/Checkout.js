86d8f0c9ad4dd676644d435f4dac5c662a4a9d30
Jeffreyx38 <Jeffreyx38@gmail.com>
2020-10-15 14:26:56 -0400

    Order submission

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-
+import {Route } from 'react-router-dom'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
-
+import ContactData from '../../containers/Checkout/ContactData/ContactData'
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 2
-        }
+        ingredients: null,
+        price: 0
+
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
-
+        let price = 0;
         const ingredients = {};
         for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1];
+            if(param[0] === 'price'){
+                price = param[1];
+            }else{
+                ingredients[param[0]] = +param[1];
+            }
+            
         }
 
-        this.setState({ ingredients: ingredients })
+        this.setState({ ingredients: ingredients, totalPrice: price })
     }
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -37,6 +39,10 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                     ingredients={this.state.ingredients} />
+                <Route 
+                path={this.props.match.path + '/contact-data'}
+                render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)}
+                 />
             </div>
         )
     }
